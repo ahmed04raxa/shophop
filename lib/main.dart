0e28e0e3cf5bbeab44ea86c6a0b45dd780f94419
ahmed04raxa <ahmed04raxa@gmail.com>
2025-10-21 23:23:18 +0500

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:shophop/firebase_options.dart';
 import 'package:shophop/repository/screens/signup/signup_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
