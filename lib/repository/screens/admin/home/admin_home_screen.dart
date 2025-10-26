8939858134588cb6957611e85c1758fee9cc3085
ahmed04raxa <ahmed04raxa@gmail.com>
2025-10-26 21:00:02 +0500

    Fetch Products Using Stream Builder

--- lib/repository/screens/admin/home/admin_home_screen.dart
+++ lib/repository/screens/admin/home/admin_home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:shophop/repository/screens/admin/products/add_product.dart';
 
@@ -13,6 +14,27 @@ class _AdminHomeScreenState extends State<AdminHomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0XFFF2F2F2),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("Products").snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return Center(child: Text("No products found"));
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text("${snapshot.data!.docs[index]['name']}"),
+              );
+            },
+          );
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
