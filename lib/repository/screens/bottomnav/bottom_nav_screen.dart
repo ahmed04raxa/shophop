3f5672b22e3864755d72192d5f6c6861d5f15144
ahmed04raxa <ahmed04raxa@gmail.com>
2025-12-28 11:38:43 +0500

    BottomNavigationScreen

--- lib/repository/screens/bottomnav/bottom_nav_screen.dart
+++ lib/repository/screens/bottomnav/bottom_nav_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:shophop/repository/screens/cart/cart_screen.dart';
+import 'package:shophop/repository/screens/home/home_screen.dart';
+import 'package:shophop/repository/screens/settings/settings_screen.dart';
+import 'package:shophop/repository/screens/wishlist/wishlist_screen.dart';
 
 class BottomNavScreen extends StatefulWidget {
   const BottomNavScreen({super.key});
@@ -8,11 +12,52 @@ class BottomNavScreen extends StatefulWidget {
 }
 
 class _BottomNavScreenState extends State<BottomNavScreen> {
+  int currentIndex = 0;
+  List pages = [HomeScreen(), WishlistScreen(), CartScreen(), SettingsScreen()];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("BOTTOM NAV SCREEN"),
+      body: pages[currentIndex],
+      bottomNavigationBar: NavigationBar(
+        backgroundColor: Color(0XFFFFFFFF),
+        indicatorColor: Color(0XFFEB3030),
+        height: 76,
+        onDestinationSelected: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        selectedIndex: currentIndex,
+        destinations: [
+          NavigationDestination(
+            icon: Image.asset(
+              "assets/images/home.png",
+              color: Color(0XFF000000),
+            ),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            selectedIcon: Image.asset("assets/images/wishlist.png"),
+            icon: Image.asset("assets/images/wishlist.png"),
+            label: 'WishList',
+          ),
+          NavigationDestination(
+            selectedIcon: Image.asset("assets/images/shopping-cart.png"),
+            icon: Image.asset(
+              "assets/images/shopping-cart.png",
+              color: Color(0XFF000000),
+            ),
+            label: 'Cart',
+          ),
+          NavigationDestination(
+            selectedIcon: Image.asset("assets/images/settings.png"),
+            icon: Image.asset(
+              "assets/images/settings.png",
+              color: Color(0XFF000000),
+            ),
+            label: 'Settings',
+          ),
+        ],
       ),
     );
   }
